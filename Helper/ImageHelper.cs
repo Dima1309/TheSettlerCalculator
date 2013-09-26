5f81ad51a60311aeaa8e72ff950970a6f5d218d5
Dmitriy Loskutov <dima1309@yandex.ru>
2013-09-26 17:42:37 +0400

    Added methods to save and load images in xml

--- Helper/ImageHelper.cs
+++ Helper/ImageHelper.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Reflection;
 using System.Windows.Media.Imaging;
+using System.Xml;
 
 namespace TheSettlersCalculator.Helper
 {
@@ -66,6 +67,54 @@ namespace TheSettlersCalculator.Helper
 			}
 
 			return decoder!=null?decoder.Frames[0]:null;
+		}
+
+		internal static void SaveToXml(XmlWriter writer, BitmapSource image)
+		{
+			MemoryStream stream = null;
+			try
+			{
+				stream = ImageToStream(image);
+				writer.WriteAttributeString("length", stream.Length.ToString());
+				writer.WriteBase64(stream.GetBuffer(), 0, (int) stream.Length);
+			}
+			finally
+			{
+				if (stream != null)
+				{
+					stream.Dispose();
+				}
+			}
+		}
+
+		internal static BitmapSource LoadFromXml(XmlReader reader)
+		{
+			string lengthStr = reader.GetAttribute("length");
+			int length = int.Parse(lengthStr);
+			byte[] buffer = new byte[length];
+			reader.ReadElementContentAsBase64(buffer, 0, length);
+			MemoryStream stream = null;
+			try
+			{
+				stream = new MemoryStream(buffer);
+				return LoadImage(stream, ".jpg");
+			}
+			finally
+			{
+				if (stream != null)
+				{
+					stream.Dispose();
+				}
+			}
+		}
+
+		private static MemoryStream ImageToStream(BitmapSource image)
+		{
+			JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+			encoder.Frames.Add(BitmapFrame.Create(image));
+			MemoryStream result = new MemoryStream();
+			encoder.Save(result);
+			return result;
 		}
 	}
 }
